43947a77f657cd46d0334f7dc58d2141126c2c5a
Vitor Paz <kngatuno@gmail.com>
2024-05-06 02:46:17 -0300

    aplicação To Do List 90% concluida

---
+++ todo/tasks/views.py
@@ -0,0 +1,100 @@
+from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Task
+from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
+from .forms import TaskForm
+from django.contrib import messages
+# Create your views here.
+
+@login_required
+def taskList(request):
+
+    search = request.GET.get('search')
+    filter = request.GET.get('filter')
+
+    if search:
+
+        tasks = Task.objects.filter(title__icontains=search, user=request.user)
+
+    elif filter:
+
+        tasks = Task.objects.filter(done=filter, user=request.user)
+
+    else:
+
+        tasks_list = Task.objects.all().order_by('-created_at').filter(user=request.user)
+
+        paginator = Paginator(tasks_list, 3)
+
+        page = request.GET.get('page')
+
+        tasks = paginator.get_page(page)
+
+    return render(request, 'tasks/list.html', {'tasks': tasks})
+
+@login_required
+def taskView(request, id):
+    task = get_object_or_404(Task, pk=id)
+    return render(request, 'tasks/task.html', {'task': task})
+
+@login_required
+def newTask(request):
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+
+        if form.is_valid():
+            task = form.save(commit=False)
+            task.done = 'doing'
+            task.user = request.user
+            task.save()
+            return redirect('/')
+    else:
+        form = TaskForm()
+        return render(request, 'tasks/addtask.html', {'form': form})
+
+@login_required
+def editTask(request, id):
+    task = get_object_or_404(Task, pk=id)
+    form = TaskForm(instance=task)
+
+    if (request.method == 'POST'):
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            task.save()
+            return redirect('/')
+        else:
+            return render(request, 'tasks/edittask.html', {'form': form, 'task': task})
+    else:
+        return render(request, 'tasks/edittask.html', {'form': form, 'task': task})
+
+@login_required
+def deleteTask(request, id):
+    task = get_object_or_404(Task, pk=id)
+    task.delete()
+
+    messages.info(request, 'Tarefa deletada com sucesso.')
+
+    return redirect('/')
+
+@login_required
+def changeStatus(request, id):
+    task = get_object_or_404(Task, pk=id)
+
+    if task.done == 'doing':
+        task.done = 'done'
+        messages.info(request, 'Tarefa Concluida.')
+    else:
+        messages.info(request, 'Tarefa em Andamento.')
+        task.done = 'doing'
+
+    task.save()
+    return redirect('/')
+
+@login_required
+def helloWorld(request):
+    return HttpResponse('Hello World!')
+
+@login_required
+def yourName(request, name):
+    return render(request, 'tasks/yourname.html', {'name': name})
