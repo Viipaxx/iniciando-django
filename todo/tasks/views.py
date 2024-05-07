2e7816368cf037d1c32062e5c60ab8bd59f985f2
Vitor Paz <kngatuno@gmail.com>
2024-05-06 22:42:42 -0300

    Projeto To Do List 100% concluido

--- todo/tasks/views.py
+++ todo/tasks/views.py
@@ -5,6 +5,7 @@ from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
 from .forms import TaskForm
 from django.contrib import messages
+import datetime
 # Create your views here.
 
 @login_required
@@ -12,6 +13,9 @@ def taskList(request):
 
     search = request.GET.get('search')
     filter = request.GET.get('filter')
+    tasksDoneRecently = Task.objects.filter(done='done', updated_at__gt=datetime.datetime.now() - datetime.timedelta(days=30), user=request.user).count()
+    tasksDone = Task.objects.filter(done='done', user=request.user).count()
+    tasksDoing = Task.objects.filter(done='doing', user=request.user).count()
 
     if search:
 
@@ -31,7 +35,7 @@ def taskList(request):
 
         tasks = paginator.get_page(page)
 
-    return render(request, 'tasks/list.html', {'tasks': tasks})
+    return render(request, 'tasks/list.html', {'tasks': tasks, 'tasksrecently': tasksDoneRecently, 'tasksdone': tasksDone, 'tasksdoing': tasksDoing})
 
 @login_required
 def taskView(request, id):
